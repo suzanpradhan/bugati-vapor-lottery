d27fc37e6d6da38add0af1b825e993f4c67772e2
Niwesh Shrestha <niweshs@gmail.com>
2023-03-11 23:39:32 +0545

    feat:dashboard divisiable by zero issue resolve

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -18,28 +18,39 @@ class DashboardController extends Controller
         $stats = Code::query()
                 ->select('id')
                 ->addSelect(['last_30' => Code::selectRaw('count(*) as total')
-                                ->whereDate('created_at', '<', now()->subDays(1))])
+                                ->whereDate('created_at', '<', now()->subDays(7))])
                 ->addSelect(['new_codes' => Code::selectRaw('count(*) as total')
-                                ->whereDate('created_at', '>=', now()->subDays(1))])
+                                ->whereDate('created_at', '>=', now()->subDays(7))])
                 ->addSelect(['scanned_codes' => Code::where('scanned', 1)->selectRaw('count(*) as total')
-                                ->whereDate('created_at', '<', now()->subDays(1))])
+                                ->whereDate('created_at', '<', now()->subDays(7))])
                 ->addSelect(['new_scanned_codes' => Code::where('scanned', 1)->selectRaw('count(*) as total')
-                                ->whereDate('created_at', '>=', now()->subDays(1))])
+                                ->whereDate('created_at', '>=', now()->subDays(7))])
                 ->addSelect(['multi_scanned_codes' => Code::where('scanned', '>', 1)->selectRaw('count(*) as total')
-                                ->whereDate('created_at', '<', now()->subDays(1))])
+                                ->whereDate('created_at', '<', now()->subDays(7))])
                 ->addSelect(['new_multi_scanned_codes' => Code::where('scanned', '>', 1)->selectRaw('count(*) as total')
-                                ->whereDate('created_at', '>=', now()->subDays(1))])
+                                ->whereDate('created_at', '>=', now()->subDays(7))])
                 ->first();
 
-        $growthPercentage = $this->calculatePercentage($stats->last_30, $stats->new_codes);
-        $scannedGrowthPercentage = $this->calculatePercentage($stats->scanned_codes, $stats->new_scanned_codes);
-        $multiScannedGrowthPercentage = $this->calculatePercentage($stats->multi_scanned_codes, $stats->new_multi_scanned_codes);
+        $last_30 = isset($stats->last_30) ? $stats->last_30 : 0;
+        $scanned_codes = isset($stats->scanned_codes) ? $stats->scanned_codes : 0;
+        $multi_scanned_codes = isset($stats->multi_scanned_codes) ? $stats->multi_scanned_codes : 0;
+
+        $new_codes = isset($stats->new_codes) ? $stats->new_codes : 0;
+        $new_scanned_codes = isset($stats->new_scanned_codes) ? $stats->new_scanned_codes : 0;
+        $new_multi_scanned_codes = isset($stats->new_multi_scanned_codes) ? $stats->new_multi_scanned_codes : 0;
+
+        $growthPercentage = $this->calculatePercentage($last_30, $new_codes);
+        $scannedGrowthPercentage = $this->calculatePercentage($scanned_codes, $new_scanned_codes);
+        $multiScannedGrowthPercentage = $this->calculatePercentage($multi_scanned_codes, $new_multi_scanned_codes);
 
         return view('dashboard.pages.dashboard', compact('totalCode', 'scannedCode', 'multScannedCode', 'recentScanned', 'growthPercentage', 'scannedGrowthPercentage', 'multiScannedGrowthPercentage'));
     }
 
     public function calculatePercentage($oldNumber, $newNumber){
         $decreaseValue =  $newNumber - $oldNumber;
+        if($decreaseValue == 0 || $oldNumber == 0) {
+            return 0;
+        }
         return ($decreaseValue / $oldNumber) * 100;
     }
 }
